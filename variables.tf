ecd8cd6bf81b915ed166cfab083ee50827d80890
Dru Jensen <drujensen@gmail.com>
2024-05-02 04:59:21 -0700

    chore: cleanup

--- variables.tf
+++ variables.tf
@@ -1,48 +1,59 @@
 variable "aws_region" {
   description = "The AWS region things are created in"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "az_count" {
   description = "Number of AZs to cover in a given region"
-  default     = "2"
+  type        = number
+  default     = 2
 }
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
+  type        = string
   default     = "bradfordhamilton/crystal_blockchain:latest"
 }
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
+  type        = number
   default     = 3000
 }
 
 variable "app_count" {
   description = "Number of docker containers to run"
+  type        = number
   default     = 3
 }
 
 variable "health_check_path" {
-  default = "/"
+  description = "Path to check for health of the service"
+  type        = string
+  default     = "/"
 }
 
 variable "fargate_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024"
+  type        = number
+  default     = 1024
 }
 
 variable "fargate_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "2048"
+  type        = number
+  default     = 2048
 }
 
 variable "project" {
   description = "The name of the project"
+  type        = string
   default     = "Example Project"
 }
 
 variable "owner" {
   description = "The owner of the resource"
+  type        = string
   default     = "Dru Jensen"
 }
